a2d3106c02372c547ec2a5b2a3d9bc3eae563503
Steinente <dawid.czech1997@gmail.com>
2023-04-03 00:04:17 +0200

    added moderation message

--- src/listeners/chat-listener.ts
+++ src/listeners/chat-listener.ts
@@ -1,6 +1,13 @@
-import { ChannelType, Client, Embed, EmbedBuilder, Events, Message, TextChannel } from 'discord.js'
+import { ChannelType, Client, Colors, Embed, EmbedBuilder, Events, Message, TextChannel } from 'discord.js'
 import Tracking from 'src/interfaces/tracking'
-import { MOVE_MEMBERS_PERMISSION, SERVER_ID, STAGE_TRACKING_CHANNEL_ID } from '../veganizer'
+import {
+  MOVE_MEMBERS_PERMISSION,
+  SERVER_ID,
+  STAGE_MODERATION_CHANNEL_ID,
+  STAGE_TRACKING_CHANNEL_ID,
+  moderationMessage,
+  reloadModerationMessage,
+} from '../veganizer'
 import { appendLog, fixMessageIfBugged, getStageChannel, manageSummary } from './interaction-listener'
 import { findTrackingIndex, trackingArray } from './voice-listener'
 
@@ -44,6 +51,47 @@ export default (client: Client): void => {
         }
       } else reply(message, 'You do not have enough permissions.')
     })
+  })
+
+  client.on(Events.MessageUpdate, async (oldMessage, newMessage) => {
+    if (oldMessage.guild?.id !== SERVER_ID) return
+    if (oldMessage.channel.id !== STAGE_TRACKING_CHANNEL_ID) return
+    if (oldMessage.channel.type !== ChannelType.GuildText) return
+    if (oldMessage.author?.id !== client.user?.id) return
+    if (oldMessage.embeds[0].color !== Colors.Red && newMessage.embeds[0].color === Colors.Red) {
+      const moderationChannel: TextChannel = client.channels.cache.get(STAGE_MODERATION_CHANNEL_ID) as TextChannel
+      await moderationChannel.bulkDelete(100).then(() => {
+        const newEmbeds = [
+          ...moderationMessage.embeds,
+          new EmbedBuilder().setDescription(`${newMessage.embeds[0].fields[0].value}: ${newMessage.url}`).data as Embed,
+        ]
+        moderationChannel
+          .send({
+            content: 'Zu moderierende Stage-Tracking-Nachrichten:',
+            embeds: newEmbeds,
+          })
+          .catch(error => {
+            if (error.code === 10008) reloadModerationMessage()
+          })
+        moderationMessage.embeds = newEmbeds
+      })
+    } else if (oldMessage.embeds[0].color === Colors.Red && newMessage.embeds[0].color !== Colors.Red) {
+      const filteredEmbeds: Embed[] = moderationMessage.embeds.filter(
+        embed => !embed.description?.includes(newMessage.url)
+      )
+      const embedsToSend: Embed[] = filteredEmbeds.length === 0 ? [] : [...filteredEmbeds]
+      await moderationMessage
+        .edit({
+          content:
+            filteredEmbeds.length === 0
+              ? 'Derzeit keine Moderation notwendig.'
+              : 'Zu moderierende Stage-Tracking-Nachrichten:',
+          embeds: embedsToSend,
+        })
+        .catch(error => {
+          if (error.code === 10008) reloadModerationMessage()
+        })
+    }
   })
 
   function reply(message: Message, value: string) {
--- src/veganizer.ts
+++ src/veganizer.ts
@@ -1,4 +1,14 @@
-import { ActivityType, Client, GatewayIntentBits, PermissionFlagsBits } from 'discord.js'
+import {
+  ActivityType,
+  Client,
+  Collection,
+  Colors,
+  EmbedBuilder,
+  GatewayIntentBits,
+  Message,
+  PermissionFlagsBits,
+  TextChannel,
+} from 'discord.js'
 import dotenv from 'dotenv'
 import ChatListener from './listeners/chat-listener'
 import InteractionListener from './listeners/interaction-listener'
@@ -13,10 +23,12 @@ export const NEW_ROLE_ID: string = process.env.NEW_ROLE_ID!
 export const TALK_ROLE_ID: string = process.env.TALK_ROLE_ID!
 export const VOID_ROLE_ID: string = process.env.VOID_ROLE_ID!
 export const STAGE_TRACKING_CHANNEL_ID: string = process.env.STAGE_TRACKING_CHANNEL_ID!
+export const STAGE_MODERATION_CHANNEL_ID: string = process.env.STAGE_MODERATION_CHANNEL_ID!
 export const MOVE_MEMBERS_PERMISSION: bigint = PermissionFlagsBits.MoveMembers
 export const MANAGE_ROLES_PERMISSION: bigint = PermissionFlagsBits.ManageRoles
 export const BAN_MEMBERS_PERMISSION: bigint = PermissionFlagsBits.BanMembers
 export const WHITE_MARK_EMOJI_ID: string = '1089867992189382667'
+export let moderationMessage: Message<true>
 
 const client = new Client({
   intents: [
@@ -37,3 +49,24 @@ client.login(process.env.TOKEN).then(() => {
   client.user!.setActivity('auf die Stage.', { type: ActivityType.Watching })
   mariaDB.connect()
 })
+
+client.on('ready', async () => reloadModerationMessage())
+
+export async function reloadModerationMessage(): Promise<void> {
+  const trackingChannel: TextChannel = client.channels.cache.get(STAGE_TRACKING_CHANNEL_ID) as TextChannel
+  const moderationChannel: TextChannel = client.channels.cache.get(STAGE_MODERATION_CHANNEL_ID) as TextChannel
+  const trackingMessages: Collection<string, Message<true>> = await trackingChannel.messages.fetch({ limit: 30 })
+  const moderationTrackingMessages: Collection<string, Message<true>> = trackingMessages.filter(
+    m => m.author.bot && m.embeds[0].color === Colors.Red
+  )
+  await moderationChannel.bulkDelete(100)
+  moderationMessage = await moderationChannel.send({
+    content:
+      moderationTrackingMessages.size === 0
+        ? 'Derzeit keine Moderation notwendig.'
+        : 'Zu moderierende Stage-Tracking-Nachrichten:',
+    embeds: moderationTrackingMessages
+      .reverse()
+      .map(message => new EmbedBuilder().setDescription(`${message.embeds[0].fields[0].value}: ${message.url}`)),
+  })
+}
