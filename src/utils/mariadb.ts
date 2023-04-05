080caf3016b73f89d5d004e3fe29b5d2af05e613
Steinente <dawid.czech1997@gmail.com>
2023-04-05 22:53:38 +0200

    added SQL-injection prevent

--- src/utils/mariadb.ts
+++ src/utils/mariadb.ts
@@ -1,6 +1,7 @@
 import { APIEmbedField, Collection, GuildMember, Message, Role, User } from 'discord.js'
 import mariadb from 'mariadb'
 import moment from 'moment'
+import Talk from 'src/interfaces/talk'
 import Tracking from 'src/interfaces/tracking'
 
 export class MariaDB {
@@ -45,14 +46,14 @@ export class MariaDB {
     })
   }
 
-  public async query(sql: string): Promise<any[]> {
+  public async query(sql: string, values?: any[]): Promise<any[]> {
     return new Promise<any[]>((resolve, reject) => {
       if (!this.dbConn) {
         reject(new Error('Not connected to database'))
         return
       }
       this.dbConn
-        .query(sql)
+        .execute(sql, values)
         .then(rows => {
           resolve(rows)
         })
@@ -63,30 +64,43 @@ export class MariaDB {
     })
   }
 
+  public async selectTalks(): Promise<Talk[]> {
+    return this.query(`SELECT * FROM talks`)
+  }
+
   public async insertTalk(message: Message, member: GuildMember): Promise<any[]> {
-    return this.query(`INSERT INTO talks
-    (message_id, user_id, user_nickname, user_tag, user_roles, message_datetime, user_banned)
-    VALUES(${message.id}, ${member.user.id}, '${member.nickname}', '${member.user.tag}', '${this.getRolesAsString(
-      member.roles.cache
-    )}', '${moment(new Date()).format('YYYY-MM-DD HH:mm:ss')}', 0)`)
+    return this.query(
+      `INSERT INTO talks
+      (message_id, user_id, user_nickname, user_tag, user_roles, message_datetime, user_banned)
+      VALUES (?, ?, ?, ?, ?, ?, 0)`,
+      [
+        message.id,
+        member.user.id,
+        member.nickname,
+        member.user.tag,
+        this.getRolesAsString(member.roles.cache),
+        moment(new Date()).format('YYYY-MM-DD HH:mm:ss'),
+      ]
+    )
   }
 
   public async updateTalkOnLeave(tracking: Tracking, timeOnStage: number): Promise<any[]> {
-    const fields: APIEmbedField[] = tracking.embedBuilder.data.fields!
-    const summary: string = fields[fields.findIndex(field => field.name === 'Log')]?.value ?? ''
-    const banList = await tracking.member.guild.bans.fetch()
-    const isBanned = banList?.some(ban => ban.user.id === tracking.member.user.id) ?? false
-    return this.query(`UPDATE talks
-    SET user_time_on_stage=${timeOnStage}
-    WHERE message_id=${tracking.message.id} AND user_id=${tracking.member.user.id}`)
+    return this.query(`UPDATE talks SET user_time_on_stage=? WHERE message_id=? AND user_id=?`, [
+      timeOnStage,
+      tracking.message.id,
+      tracking.member.user.id,
+    ])
   }
 
   public async updateTalkOnSummary(message: Message, targetUserId: string, interactionUser: User): Promise<any[]> {
     const fields: APIEmbedField[] = message.embeds[0].fields
     const summaryIndex: number = fields.findIndex(field => field.name.startsWith('Summary by '))
-    return this.query(`UPDATE talks
-    SET summary='${fields[summaryIndex]?.value}', last_summary_mod_id=${interactionUser.id}
-    WHERE message_id=${message.id} AND user_id=${targetUserId}`)
+    return this.query(`UPDATE talks SET summary=?, last_summary_mod_id=? WHERE message_id=? AND user_id=?`, [
+      fields[summaryIndex]?.value,
+      interactionUser.id,
+      message.id,
+      targetUserId,
+    ])
   }
 
   public async updateTalkOnRoleChange(
@@ -95,25 +109,29 @@ export class MariaDB {
     interactionMember: GuildMember,
     buttonName: string
   ): Promise<any[]> {
-    return this.query(`UPDATE talks
-    SET user_roles='${this.getRolesAsString(targetMember.roles.cache)}', last_${buttonName}_mod_id=${
-      interactionMember.user.id
-    }
-    WHERE message_id=${message.id} AND user_id=${targetMember.user.id}`)
+    return this.query(`UPDATE talks SET user_roles=?, last_${buttonName}_mod_id=? WHERE message_id=? AND user_id=?`, [
+      this.getRolesAsString(targetMember.roles.cache),
+      interactionMember.user.id,
+      message.id,
+      targetMember.user.id,
+    ])
   }
 
   public async updateTalkOnBan(message: Message, targetUserId: string, interactionMember: GuildMember): Promise<any[]> {
     const banList = await interactionMember.guild.bans.fetch()
     const isBanned = banList?.some(ban => ban.user.id === targetUserId) ?? false
-    return this.query(`UPDATE talks
-    SET user_banned=${isBanned}, last_ban_mod_id=${interactionMember.user.id}
-    WHERE message_id=${message.id} AND user_id=${targetUserId}`)
+    return this.query(`UPDATE talks SET user_banned=?, last_ban_mod_id=? WHERE message_id=? AND user_id=?`, [
+      isBanned,
+      interactionMember.user.id,
+      message.id,
+      targetUserId,
+    ])
   }
 
-  public async selectTalkCountByUser(targetUserId: string): Promise<any[]> {
-    return this.query(`SELECT count(message_id)
-    FROM talks
-    WHERE user_id=${targetUserId} AND user_time_on_stage >= 60`)
+  public async selectTalkCountByUserId(targetUserId: string): Promise<any[]> {
+    return this.query(`SELECT count(message_id) FROM talks WHERE user_id=? AND user_time_on_stage >= 60`, [
+      targetUserId,
+    ])
   }
 
   private getRolesAsString(roles: Collection<string, Role>): string {
